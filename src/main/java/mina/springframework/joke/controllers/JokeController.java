c9b19ff1ba3bf4af49aea0af090a9f486b45735f
minarazi <razimina76@gmail.com>
2018-04-27 23:59:29 +0430

    Initial commit

---
+++ src/main/java/mina/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,31 @@
+package mina.springframework.joke.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import mina.springframework.joke.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	@Autowired
+	public JokeController(JokeService jokeService) {
+		
+		this.jokeService = jokeService;
+	}
+
+	@RequestMapping({"/",""})
+	public String ShowJoke(Model model) {
+		
+		model.addAttribute( "joke",jokeService.getJoke());
+		
+		return "chucknorris";
+	}
+	
+	
+	
+}
---
+++ src/main/java/mina/springframework/joke/services/JokeService.java
@@ -0,0 +1,6 @@
+package mina.springframework.joke.services;
+
+public interface JokeService{
+	
+	String getJoke();
+}
